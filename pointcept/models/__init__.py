78cf6db8d4d4b18fba370128aaae2f4d6ec6a047
aditya <medhe.aditya@gmail.com>
2024-02-23 18:52:26 +0530

    intermediate commit

--- pointcept/models/__init__.py
+++ pointcept/models/__init__.py
@@ -16,7 +16,7 @@ from .spvcnn import *
 from .context_aware_classifier import *
 
 # Instance Segmentation
-# from .point_group import *
+from .point_group import *
 
 # Pretraining
 from .masked_scene_contrast import *
